0c43272d20487c532794148f4b393e1b51fabe28
Heresy <kheresy@gmail.com>
2018-09-09 20:52:01 +0800

    Re-allocate files

---
+++ Example/example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "HttpClient.h"
+
+int main(int argc, char** argv )
+{
+	if (argc < 2)
+	{
+		std::cerr << "Please give a web URL" << std::endl;
+		return -1;
+	}
+
+	HttpClient	mClient;
+	mClient.m_sigErrorLog.connect([](const std::string& sError) {
+		std::cout << "[Error] " << sError << std::endl;
+	});
+
+	boost::optional<std::wstring> bHtml = mClient.ReadHtml( argv[1] );
+	if (bHtml)
+	{
+		std::wcout << bHtml.value();
+	}
+	else
+	{
+		std::cout << "Error" << std::endl;
+	}
+}
